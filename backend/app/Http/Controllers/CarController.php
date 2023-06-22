ad361887c75a5458f1014504f55cf83255fb34d4
Guilherme Vargas <gvargas159z@gmail.com>
2023-06-22 18:48:06 -0300

    BackEnd Cars

---
+++ backend/app/Http/Controllers/CarController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Cars;
+
+class CarController extends Controller
+{
+    public function createCar(Request $request){
+        $car = new Cars;
+        $car->marca = $request-> input('marca');
+        $car->modelo = $request-> input('modelo');
+        $car->ano = $request-> input('ano');
+        $car->cor = $request-> input('cor');
+
+        if( $car->save()) {
+            return $car;
+        }
+    }
+
+    public function getCarByID($id)
+    {
+        return Cars::findOrFail($id);
+    }
+
+    public function getAllCars()
+    {
+        return Cars::all();
+    }
+
+    public function update(Request $request, $id)
+    {
+        $car = Cars::findOrFail( $request->id);
+        $car->marca = $request-> input('marca');
+        $car->modelo = $request-> input('modelo');
+        $car->ano = $request-> input('ano');
+        $car->cor = $request-> input('cor');
+
+        if( $car->save()) {
+            return $car;
+        }
+    }
+
+    public function delete($id)
+    {
+        $car = Cars::findOrFail( $id );
+        if( $car->delete() ) {
+            return $car;
+        };
+    }
+}
